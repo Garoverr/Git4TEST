b43544c92ea245ded332046a2b1d494a26659ca4
Garoverr <jacques.sapin@epitech.eu>
2024-04-04 10:31:45 +0200

    [Add] func printing hello world

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** 
+** File description:
+** 
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main(__attribute__((unused))int argc, __attribute__((unused)) char *argv[])
+{
+    write(1, "Hello World !",13); 
+    return 0;
+}
